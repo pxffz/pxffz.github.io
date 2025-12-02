8f37544ee601c0b5c0aeb2a77f9ab36c347a08af
pxffzz <ansaranwari1234@gmail.com>
2025-12-02 10:08:28 -0500

    Update uv.config.js

--- active/uv/uv.config.js
+++ active/uv/uv.config.js
@@ -1,6 +1,6 @@
 self.__uv$config = {
     prefix: '/active/go/',
-    bare:'https://qms.is.great.and.fun.colloky.com.pe/bare/',
+    bare:'https://qms.is.colloky.com.pe/bare/',
     encodeUrl: Ultraviolet.codec.xor.encode,
     decodeUrl: Ultraviolet.codec.xor.decode,
     handler: '/active/uv/uv.handler.js',
